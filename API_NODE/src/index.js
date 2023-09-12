2a121155b905c67349b5888e3775896dfa6e77f6
TAMER  SERHAN <tamer.srhn@gmail.com>
2023-09-11 23:07:40 -0300

    adicionando pontos do node

---
+++ API_NODE/src/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import mysql from 'mysql';
+
+//configurações mysql //
+
+const connection = mysql.createConnection({
+    host: '172.17.0.2',
+    user: 'root',
+    password: 'databasesql',
+    database: 'DATALAKE_SQL'
+});
+
+connection.connect();
+
+// configurações node API express //
+
+const app = express();
+
+app.listen(9001, '0.0.0.0', chamadaInitial())
+
+app.get('/products', function(req,res){
+    connection.query(
+        'SELECT * FROM products', 
+        function(error, results){
+            if (error){
+                throw error
+            };
+
+            res.send(
+                results.map(
+                    item => ({
+                    name: item.name,
+                    price: item.price
+                    })
+                )
+            )
+        }
+    )
+})
+
+
+function chamadaInitial(){
+    console.log(" Escutando a porta 9001")
+}
+
