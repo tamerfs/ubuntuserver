04ac2047897eaa03fffdb12371675f49065c5019
TAMER  SERHAN <tamer.srhn@gmail.com>
2023-09-11 22:23:30 -0300

    update sql scripts

--- API_NODE/db/initial_script_database.sql
+++ API_NODE/db/initial_script_database.sql
@@ -7,7 +7,3 @@ CREATE TABLE IF NOT EXISTS products (
     price DECIMAL(10, 2),
     PRIMARY KEY (id)
 );
-
-INSERT INTO products VALUE(0, 'produto_1', 2500);
-INSERT INTO products VALUE(0, 'produto_2', 5500);
-INSERT INTO products VALUE(0, 'produto_3', 1700);
