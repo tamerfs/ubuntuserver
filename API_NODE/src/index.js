21389594ef6173ccbbd5d8e8f4e51aaffc91a3f1
TAMER  SERHAN <tamer.srhn@gmail.com>
2023-09-15 18:33:02 -0300

    colcoado mais um endpoint

--- API_NODE/src/index.js
+++ API_NODE/src/index.js
@@ -8,6 +8,7 @@ const app = express()
 const PORT = 9001
 const HOST = '0.0.0.0'
 const INIT_DIR = '/products'
+const USER_DIR = '/users'
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`)
@@ -15,14 +16,39 @@ app.listen(PORT, HOST, () => {
 
 app.get('/', (req, res) => {
   res.send(`
-  <h1>Node server running on Docker<h1/> <br>
-  <h4> Acesse ao <a target="_blank" href="${INIT_DIR}">LINK<a/><h4/>
+  <h1> Node server API running on Docker <h1/> <br>
+  <h2> Tal link acessado é por meio de um tunnel via NGROK<h2/> <br>
+  <h3> Aplicação rodando em um container cp, node-16<h3/> <br>
+  <h3> Interligado via conexão e direcionamento de porta com uma isntancia MySQL<h3/> <br>
+  <h4> Acesse ao request Products<a target="_blank" href="${INIT_DIR}">LINK<a/><h4/>
+  <h4> Acesse ao request Users<a target="_blank" href="${USER_DIR}">LINK<a/><h4/>
    `)
 })
 
 app.get(INIT_DIR, (req, res) => {
   connectionDb.query(
     'SELECT * FROM products',
+    (error, results) => {
+      if (error) {
+        console.log('erro na arrow function - Erro ao carregar a query')
+        res.send('<h1>Erro ao carregar a query<h1/>')
+        throw error
+      };
+      res.send(
+        results.map(
+          item => ({
+            name: item.name,
+            price: item.price
+          })
+        )
+      )
+    }
+  )
+})
+
+app.get(USER_DIR, (req, res) => {
+  connectionDb.query(
+    'SELECT * FROM application_user',
     (error, results) => {
       if (error) {
         console.log('erro na arrow function - Erro ao carregar a query')
