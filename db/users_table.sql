485c07b1ae6c0671c6934ad0313d598502c1fecf
TAMER  SERHAN <tamer.srhn@gmail.com>
2023-09-15 18:11:20 -0300

    trocado ENCRYPT por PASSWORD()

--- db/users_table.sql
+++ db/users_table.sql
@@ -2,16 +2,18 @@ CREATE TABLE IF NOT EXISTS application_user(
     -- uuid uuid DEFAULT uuid_generate_v4(),
     id BINARY(16) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
+    -- user_last_name VARCHAR(255) NOT NULL,
+    -- user_first_name VARCHAR(255) NOT NULL,
     -- cpf/cnpj VARCHAR(255) NOT NULL,
     -- email VARCHAR(255),
     -- data_nascimento DATE,
     password VARCHAR(255) NOT NULL
 )
 
+SELECT UUID() AS userid;
+
 SELECT * from application_user
 
-INSERT INTO application_user (id, username, password) 
-    VALUES 
-        (UUID_TO_BIN(UUID()),'tamer', ENCRYPT('tamer1914','my_salt')),
-        (UUID_TO_BIN(UUID()),'admin', ENCRYPT('admin1918','my_salt')),
-        (UUID_TO_BIN(UUID()),'samuel', ENCRYPT('samuel1995','my_salt'));
+INSERT INTO application_user (id, username, password) VALUES (UUID_TO_BIN(UUID()),'tamer', PASSWORD('tamer1914','my_salt'));
+INSERT INTO application_user (id, username, password) VALUES (UUID_TO_BIN(UUID()),'admin', PASSWORD('admin1918','my_salt'));
+INSERT INTO application_user (id, username, password) VALUES (UUID_TO_BIN(UUID()),'samuel', PASSWORD('samuel1995','my_salt'));
